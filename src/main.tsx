42e25bf82b9df0166ae64282b7b35bd96d0df2b3
Reacher <macryze@icloud.com>
2025-01-09 00:57:10 +0800

    Add React Buddy integration and dev environment updates
    Integrated "@react-buddy/ide-toolbox" for improved development experience, including support for previews and initialization hooks. Updated `.idea` configuration, `vite.config.ts`, and the `.gitignore` file to reflect changes. Introduced a `.env` file and set `FAST_REFRESH=false` to enhance development consistency.

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,18 @@ import { Provider } from "@/components/ui/provider";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { DevSupport } from "@react-buddy/ide-toolbox";
+import { ComponentPreviews, useInitial } from "@/dev";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider>
-      <App />
+      <DevSupport
+        ComponentPreviews={ComponentPreviews}
+        useInitialHook={useInitial}
+      >
+        <App />
+      </DevSupport>
     </Provider>
   </React.StrictMode>,
 );
--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,5 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  server: { host: "127.0.0.1" },
 });
