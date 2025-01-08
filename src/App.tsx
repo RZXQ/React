2d06a62082a20d42030a55104f09dbbff638678e
Reacher <macryze@icloud.com>
2025-01-08 20:42:23 +0800

    Refactor ListGroup component and update import path
    Moved the ListGroup component to a new directory focusing on rendering lists. Updated the import path in App.tsx to reflect this change and added a simple implementation to display a list of cities.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import ListGroup from "@/part-1/3- Building Components/3- Fragments/ListGroup.tsx";
+import ListGroup from "@/part-1/3- Building Components/4- Rendering Lists/ListGroup.tsx";
 
 function App() {
   return <ListGroup />;
