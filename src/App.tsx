a4c3331cb7cd6223c2da44b27396293a90d93b9a
Reacher <macryze@icloud.com>
2025-01-08 20:13:25 +0800

    Refactor ListGroup component and update import path
    Moved the ListGroup component to a new folder for better organization and updated the import path in App.tsx. Added a new ListGroup implementation using Chakra UI elements with a Heading and list items wrapped in a fragment.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import ListGroup from "./part-1/3- Building Components/2- Creating a ListGroup Component/components/ListGroup.tsx";
+import ListGroup from "@/part-1/3- Building Components/3- Fragments/ListGroup.tsx";
 
 function App() {
   return <ListGroup />;
